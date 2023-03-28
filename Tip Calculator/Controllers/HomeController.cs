871d9c13fd5491ee1d67c2d0e66d3f252baf1084
kelec <kelec@LAPTOP-JGFOV90A>
2023-03-28 15:06:41 -0400

    Add project files.

---
+++ Tip Calculator/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tip_Calculator.Models;
+
+namespace Tip_Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.A = 0;
+            ViewBag.B = 0;
+            ViewBag.C = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.A = model.CalculateFifteen();
+                ViewBag.B = model.CalculateTwenty();
+                ViewBag.C = model.CalculateTwentyFive();
+            }
+            else
+            {
+                ViewBag.A = 0;
+                ViewBag.B = 0;
+                ViewBag.C = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Tip Calculator/Models/TipCalculatorModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tip_Calculator.Models
+{
+    public class TipCalculatorModel
+    {
+        [Required(ErrorMessage = "Please enter a meal price.")]
+        [Range(1, 500, ErrorMessage =
+              "The meal price must be greater than 0 and less than 500")]
+        public decimal? SalesPrice { get; set; }
+
+        public decimal? CalculateFifteen()
+        {
+            return SalesPrice * 0.15m;
+        }
+        public decimal? CalculateTwenty()
+        {
+            return SalesPrice * 0.20m;
+        }
+        public decimal? CalculateTwentyFive()
+        {
+            return SalesPrice * 0.25m;
+        }
+    }
+}
